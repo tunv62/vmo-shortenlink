9b5004486fe9876e41d434ab069026a076fd35da
nguyen tu <tunv62@wru.vn>
2020-09-17 11:32:23 +0700

    init models, add relationship

---
+++ models/shortlink.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const linkSchema = mongoose.Schema({
+    creator: String,
+    longUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: String,
+    expire: Date,
+    isExpire: {
+        type: Boolean,
+        default: false
+    },
+    createAt: {
+        type: Date,
+        default: new Date()
+    },
+    clicks: {
+        type: Number,
+        default: 0
+    },
+    isBlock: {
+        type: Boolean,
+        default: false
+    },
+    blockBy: String,
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date
+})
+
+const shortLink = mongoose.model('shortLink', linkSchema)
+
+module.exports = shortLink
