67eba0cbdb676b96421cc3f6a5e5661e5f465607
nguyen tu <tunv62@wru.vn>
2020-10-14 16:01:13 +0700

    update maxAge Session

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
     secret: process.env.SESSION_SECRET,
     cookie: {
-        maxAge: 5 * 60 * 1000
+        maxAge: 10 * 24 * 60 * 60 * 1000
     }
 }))
 app.use(flash())
